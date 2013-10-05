863c19dd9afabb72bb8a807b608f2968976c91d9
Sam <sam@ozchamp.net>
2013-10-05 10:52:13 +0800

    folder check addon

--- catalog/controller/debug/debug.php
+++ catalog/controller/debug/debug.php
@@ -6,14 +6,19 @@ class ControllerDebugDebug extends Controller {
 
 	private $error = array();
 
-	public function index() {
+	public function index(){
+		if(! $this->validate()){
+			$this->redirect(HTTP_PATH);
+			exit;
+		}
+	}
+
+	public function mail() {
 		if(! $this->validate()){
 			$this->redirect(HTTP_PATH);
 			exit;
 		}
 
-		$to = array('cmd.dos@hotmail.com','mail.song.de.qiang@gmail.com');
-		$sender = '千點距';
 		$subject = 'OpenCart郵件測試-主題';
 		$html = '<h1>OpenCart郵件測試</h1>OpenCart郵件測試內容<br/>OpenCart郵件測試內容<br/><br/>OpenCart郵件測試內容<br/>OpenCart郵件測試內容<br/>';
 
@@ -25,13 +30,10 @@ class ControllerDebugDebug extends Controller {
 		$mail->Port = $this->config->get('config_smtp_port');
 		$mail->Timeout = $this->config->get('config_smtp_timeout');
 
+		$mail->Sender = $this->config->get('config_smtp_username');
 
-		$mail->SetFrom($this->config->get('config_smtp_username'), $this->config->get('config_name'));
-		$mail->ClearReplyTos();
-		$mail->AddReplyTo($this->config->get('config_email'), $this->config->get('config_name'));
-		foreach($to as $addr){
-			$mail->AddAddress($addr);
-		}
+		$mail->SetFrom('cmd.dos@hotmail.com', $this->config->get('config_name'));
+		$mail->AddAddress('sam@ozchamp.net');
 	    $mail->Subject = html_entity_decode($subject, ENT_QUOTES, 'UTF-8');
 	    $mail->MsgHTML(html_entity_decode($html, ENT_QUOTES, 'UTF-8'));
 	    $mail->Send();
@@ -41,6 +43,38 @@ class ControllerDebugDebug extends Controller {
 	    }
 
 		//$this->response->setOutput($output);
+  	}
+
+  	public function folder(){
+  		if(! $this->validate()){
+			$this->redirect(HTTP_PATH);
+			exit;
+		}
+
+		$folders = array(
+			DIR_IMAGE,
+			DIR_CACHE,
+			DIR_DOWNLOAD,
+			DIR_LOGS,
+		);
+
+		$results = array();
+
+		foreach($folders as $folder){
+			$results[] = array(
+				$folder,
+				(int)is_dir($folder),
+				is_dir($folder) ? substr(sprintf('%o', fileperms($folder)), -4) : '0000',
+			);
+		}
+
+		$html = '<table width="100%">';
+		foreach($results as $result){
+			$html .= str_replace(array('{Folder}', '{IsDir}', '{Perms}'), $result, '<tr><td>{Folder}</td><td>{IsDir}</td><td>{Perms}</td></tr>');
+		}
+		$html.='</table>';
+
+		echo $html;
   	}
 
   	private function validate(){
